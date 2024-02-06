b6a4a3a2d739b13c45d5ddc5f3f0acdf6abf83de
Ray Hsiao <40257341+rh5140@users.noreply.github.com>
2024-02-06 13:54:12 -0800

    janky screenshot test

---
+++ DMA 172/Assets/Scripts/CaptureImage.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CaptureImage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    //https://gamedevbeginner.com/how-to-capture-the-screen-in-unity-3-methods/
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Tab))
+        {
+            StartCoroutine(TakeScreenshot());
+        }
+    }
+
+    IEnumerator TakeScreenshot()
+    {
+        yield return new WaitForEndOfFrame();
+        ScreenCapture.CaptureScreenshot("screenshot.png"); // need to have unique filename so distinguishable
+        Debug.Log("a screenshot was taken!");
+    }
+}
